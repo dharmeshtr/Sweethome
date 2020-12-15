04dd0016d4ae4be8afe68404f2d1003be430acb9
Dharmesh <dRana4SFDC@gmail.com>
2020-12-14 21:20:46 -0500

    initial commit

---
+++ AbstractTrial.java
@@ -0,0 +1,14 @@
+abstract class AbstractTrial 
+{
+	private int eno;
+	private String ename;
+
+	public void displayEmp() {
+		System.out.println("I am in display");
+	}
+	
+	public static void main(String args[]) {
+		System.out.println("AbstractTrial");
+		//AbstractTrial abt = new AbstractTrial();
+	}
+}
